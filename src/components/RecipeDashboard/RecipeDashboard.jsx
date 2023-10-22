3a01f77919fa91f6fd066baf890a855f0e5cdd5e
lavenderYuu <lavenderyu0113@gmail.com>
2023-10-21 17:21:31 -0700

    Simplify the code
    - Simplify the card by passing the parameter in card
    - add some css to navbar

--- src/components/RecipeDashboard/RecipeDashboard.jsx
+++ src/components/RecipeDashboard/RecipeDashboard.jsx
@@ -1,23 +1,47 @@
-import React from 'react';
-import './styles.css';
-import AppetizerCard from '../RecipeCard/AppetizerCard';
-import DessertCard from '../RecipeCard/DessertCard';
-import MainCourseCard from '../RecipeCard/MainCourseCard';
-import SoupCard from '../RecipeCard/SoupCard';
+import React from "react";
+import "./styles.css";
+import RecipeCard from "../RecipeCard/RecipeCard";
+
+import soupIcon from "../../assets/soupIcon.png";
+import mainCourseIcon from "../../assets/mainCourseIcon.png";
+import dessertIcon from "../../assets/dessertIcon.png";
+import appetizerIcon from "../../assets/appetizerIcon.png";
 
 function RecipeDashboard({}) {
   return (
     <div className="container">
-        <div className="card-layout">
-            {/* RecipeCard components go here */}
-            <AppetizerCard />
-            <MainCourseCard />
-            <div className="search-box-wrapper">
-                <input type="text" placeholder="Search for a recipe..." className="search-box" />
-            </div>
-            <SoupCard />
-            <DessertCard />
+      <div className="card-layout">
+
+      <div className="search-box-wrapper">
+          <input
+            type="text"
+            placeholder="Search for a recipe..."
+            className="search-box"
+          />
         </div>
+        
+        <RecipeCard
+          title="Appetizer"
+          description="I love Appetizer"
+          img_path={appetizerIcon}
+        />
+        <RecipeCard
+          title="Main Course"
+          description="I love Main Course"
+          img_path={mainCourseIcon}
+        />
+        
+        <RecipeCard
+          title="Soup"
+          description="I love Soup"
+          img_path={soupIcon}
+        />
+        <RecipeCard
+          title="Dessert"
+          description="I love Dessert"
+          img_path={dessertIcon}
+        />
+      </div>
     </div>
   );
 }
