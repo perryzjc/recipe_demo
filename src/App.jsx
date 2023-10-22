760c118f089d448bcb4b37fd92440e392148d5e3
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2023-10-22 01:49:14 -0700

    Refract location of Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./pages/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import Appetizer from "./pages/Appetizer";
 import MainCourse from "./pages/MainCourse";
