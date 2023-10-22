89109379874bad1e1bf5edcd2719ceceb2cf8dda
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2023-10-21 20:34:22 -0700

    Update navbar and logo

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import './Navbar.css';
+import recipeIcon from '../assets/logo.png';
 
 function Navbar() {
   const linkVariants = {
-    hover: { scale: 1.1, originX: 0, color: "#e44d26" },
+    hover: { scale: 1.05, originX: 0, color: "#ffffff" },
     tap: { scale: 0.95 }
   };
 
   return (
-    <motion.nav className="navbar" initial={{ y: -250 }} animate={{ y: 0 }} transition={{ type: 'spring', stiffness: 120 }}>
+    <motion.nav className="navbar" initial={{ y: -250 }} animate={{ y: 0 }} transition={{ type: 'spring', stiffness: 120, damping: 20 }}>
       <div className="navbar-container">
-        <Link to="/recipe_demo/" className="navbar-title">Food Blog</Link>
+        <Link to="/recipe_demo/" className="navbar-title">
+        <img src={recipeIcon} alt="Recipe" className="navbar-icon" />
+          Recepi Blog
+        </Link>
         <ul className="navbar-list">
           {['Appetizer', 'Main Course', 'Soup', 'Dessert'].map(item => (
             <li className="nav-item" key={item}>
