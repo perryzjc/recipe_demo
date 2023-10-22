0f3e808789b86b39c55c6e05a4da26bd8f79ecfa
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2023-10-21 20:01:06 -0700

    Backup

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,41 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import './Navbar.css';
 
 function Navbar() {
-  return (
-    <>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/recipe_demo/" className="navbar-title">
-            Food Blog
-          </Link>
+  const linkVariants = {
+    hover: { scale: 1.1, originX: 0, color: "#e44d26" },
+    tap: { scale: 0.95 }
+  };
 
-          <ul className="navbar-list">
-            <li className="nav-item">
-              <Link to="/recipe_demo/appetizer" className="nav-links">
-                Appetizer
-              </Link>
+  return (
+    <motion.nav className="navbar" initial={{ y: -250 }} animate={{ y: 0 }} transition={{ type: 'spring', stiffness: 120 }}>
+      <div className="navbar-container">
+        <Link to="/recipe_demo/" className="navbar-title">Food Blog</Link>
+        <ul className="navbar-list">
+          {['Appetizer', 'Main Course', 'Soup', 'Dessert'].map(item => (
+            <li className="nav-item" key={item}>
+              <motion.div variants={linkVariants} whileHover="hover" whileTap="tap">
+                <Link to={`/recipe_demo/${item.toLowerCase()}`} className="nav-links">{item}</Link>
+              </motion.div>
             </li>
-            <li className="nav-item">
-              <Link to="/recipe_demo/maincourse" className="nav-links">
-                Main Course
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/recipe_demo/soup" className="nav-links">
-                Soup
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/recipe_demo/dessert" className="nav-links">
-                Dessert
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </>
+          ))}
+        </ul>
+      </div>
+    </motion.nav>
   );
 }
 
