46b64e72d58e39b272724ccfa83fa8df0217f1e9
lavenderYuu <lavenderyu0113@gmail.com>
2023-10-23 14:23:35 -0700

    Change some style
    - change the background and style

--- src/components/RecipeInstruction/RecipeInstruction.jsx
+++ src/components/RecipeInstruction/RecipeInstruction.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import './RecipeInstruction.css';
+import React from "react";
+import "./RecipeInstruction.css";
 
 const RecipeInstruction = ({ recipeName, recipeImage, instructions }) => {
-    const backgroundStyle = {
-        backgroundImage: `linear-gradient(rgba(4, 9, 30, 0.7), rgba(4, 9, 30, 0.7)), url(${recipeImage})`
-    };
+  const backgroundStyle = {
+    backgroundImage: `linear-gradient(rgba(4, 9, 30, 0.7), rgba(4, 9, 30, 0.7)), url(${recipeImage})`,
+  };
 
-    return (
-        <div className="recipe-instruction-page" style={backgroundStyle}>
-            <h2 className="recipe-name">{recipeName}</h2>
-            <div className="recipe-instruction-container">
-                {instructions.map((instruction, index) => (
-                    <div className="instruction-row" key={index}>
-                        <div className="step-content">Step {index + 1}</div>
-                        <div className="instruction-content">{instruction}</div>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="recipe-instruction-page" style={backgroundStyle}>
+      <h2 className="recipe-name">{recipeName}</h2>
+      <div className="recipe-instruction-container">
+        {/* <img src=url(${recipeImage}) alt="" className='dishimg'/> */}
+        {instructions.map((instruction, index) => (
+          <div className="instruction-row" key={index}>
+            <div className="step-content">Step {index + 1}</div>
+            <div className="instruction-content">{instruction}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default RecipeInstruction;
