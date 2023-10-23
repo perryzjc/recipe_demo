7547b9ffe9f27cbc91d6438d26ee00335cd2fe81
Portia Chung <portiachung@Portias-MacBook-Pro.local>
2023-10-22 22:03:23 -0700

    Add recipe contents to src/pages/Contents.jsx

--- src/pages/Contents.jsx
+++ src/pages/Contents.jsx
@@ -1,75 +1,264 @@
+
+
+
 export const contents = [
     {
         id: 1,
         category: "Appetizer",
-        title: "Appetizer 1",
-        image: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=pexels-ella-olsson-1640772.jpg&fm=jpg",
-        article: "Add anything you want here!",
+        articleTitle: "Ham ‘n’ Cheese Biscuit Stacks",
+        imagePath: "src/assets/recipe/HamCheeseBiscuitStacks.jpg",
+        author: "Kelly Williams",
+        datePublished: "Sep. 07, 2022",
+        description: "These finger sandwiches are filling enough to satisfy hearty appetites. I've served the fun little stacks at every event, including holiday gatherings, showers and tailgate parties.",
+        ingredients: {
+            primary: [
+                "4 tubes (6 ounces each) small refrigerated flaky biscuits (5 count each)",
+                "1/4 cup stone-ground mustard",
+                "1/4 cup honey",
+                "10 thick slices deli ham, quartered",
+                "10 slices Swiss cheese, quartered",
+                "2-1/2 cups shredded romaine",
+                "20 pitted ripe olives, drained and patted dry",
+                "20 pimiento-stuffed olives, drained and patted dry",
+                "40 frilled toothpicks"
+            ],
+            assembly: [
+                "1/2 cup butter, softened",
+                "1/4 cup chopped green onions",
+                "1/2 cup stone-ground mustard",
+                "1/4 cup mayonnaise"
+            ],
+        },
+        instructions: [
+            "Step1. Preheat oven to 400°. Cut biscuits in half to make half-circles; place 2 in. apart on ungreased baking sheets. Spread mustard over tops. Bake until golden brown, 8-10 minutes. Cool completely on wire racks.",
+            "Step2. Mix butter and green onions. In another bowl, mix mustard, mayonnaise and honey. Split each biscuit into 2 layers.",
+            "Step3. Spread biscuit bottoms with butter mixture; top with ham, cheese, romaine and biscuit tops. Spoon mustard mixture over tops. Thread 1 olive onto each toothpick; insert into stacks. Serve immediately."
+        ],
+
         recipe: "Recipe for Appetizer 1",
         // can add other field here (EX: steps? (can make a array), author, brabrabra)
+
     },
+
     {
         id: 2,
         category: "Appetizer",
-        title: " Appetizer 2",
-        image: "https://media.istockphoto.com/id/1457433817/photo/group-of-healthy-food-for-flexitarian-diet.jpg?s=612x612&w=0&k=20&c=v48RE0ZNWpMZOlSp13KdF1yFDmidorO2pZTu2Idmd3M=",
-        article: "Add anything you want here!",
+        articleTitle: "Sweet Gingered Chicken Wings",
+        imagePath: "src/assets/recipe/SweetGingeredChickenWings.png",
+        author: "Debbie Dougal",
+        datePublished: "Sep. 24, 2022",
+        description: "I first tasted this delicious chicken dish when I attended a class on using honey in cooking. When I prepare this recipe for a party, it's one of the first dishes to disappear!",
+        ingredients: {
+            primary: [
+                "1 cup all-purpose flour",
+                "2 teaspoons salt",
+                "2 teaspoons paprika",
+                "1/4 teaspoon pepper",
+                "24 chicken wings (about 5 pounds)"
+
+            ],
+            sauce: [
+                "1/4 cup honey",
+                "1/4 cup thawed orange juice concentrate",
+                "1/2 teaspoon ground ginger",
+                "Minced fresh parsley, optional"
+            ],
+        },
+        instructions: [
+            "Step1. Preheat oven to 350°. Line 2 baking sheets with foil; coat with cooking spray.",
+            "Step2. In a shallow dish, combine flour, salt, paprika and pepper. Add chicken wings, a few at a time; toss to coat. Divide wings between prepared pans. Bake 30 minutes.",
+            "Step3. In a small bowl, combine honey, orange juice concentrate and ginger; brush over chicken wings. Bake until juices run clear, 25-30 minutes.",
+            "Step4. Preheat broiler. Broil wings 4 in. from heat until lightly browned, 1-2 minutes. If desired, sprinkle with parsley."
+        ],
+
         recipe: "Recipe for Appetizer 2",
         // can add other field here (EX: steps? (can make a array), author, brabrabra)
+
     },
+
+
     {
         id: 3,
         category: "MainCourse",
-        title: "MainCourse 1",
-        image: "https://www.foodandwine.com/thmb/20Fp26wbzSMgOtCdCZy0vpIK8Mo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/garlic-butter-prime-rib-FT-RECIPE0621-fca73e5fa8e046b0b03982757db51628.jpg",
-        article: "Add anything you want here!",
+        articleTitle: "Cast-Iron Skillet Steak",
+        imagePath: "src/assets/recipe/CastIronSkilletSteak.png",
+        author: "James Schend, Dairy Freed",
+        datePublished: "Oct. 21, 2023",
+        description: "If you’ve never cooked steak at home before, it can be a little intimidating. That’s why I came up with this simple steak recipe that’s so easy, you could make it any day of the week.",
+        ingredients: {
+            primary: [
+                " beef New York strip or ribeye steak (1 pound), 1 inch thick",
+                "3 teaspoons kosher salt, divided"
+            ],
+        },
+        instructions: [
+            "Step1. Remove steak from refrigerator and sprinkle with 2 teaspoons salt; let stand 45-60 minutes.",
+            "Step2. Preheat a cast-iron skillet over high heat until extremely hot, 4-5 minutes. Sprinkle remaining 1 teaspoon salt in bottom of skillet; pat beef dry with paper towels. Place steak in skillet and cook until easily moved, 1-2 minutes; flip, placing steak in a different section of the skillet. Cook 30 seconds and then begin moving steak, occasionally pressing slightly to ensure even contact with skillet.",
+            "Step3. Continue turning and flipping until cooked to desired degree of doneness (for medium-rare, a thermometer should read 135°; medium, 140°; medium-well, 145°), 1-2 minutes."
+        ],
+
+
+
         recipe: "Recipe for MainCourse 1",
         // can add other field here (EX: steps? (can make a array), author, brabrabra)
+
     },
+
     {
         id: 4,
         category: "MainCourse",
-        title: "MainCourse 2",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOd13nFLnRXoOs9VlsIYJzkaHi5pxs2LWn9Q&usqp=CAU",
-        article: "Add anything you want here!",
+        articleTitle: "Pesto Lasagna",
+        imagePath: "src/assets/recipe/PestoLasagna.png",
+        author: "Taste of Home Test Kitchen",
+        datePublished: "Sep. 13, 2023",
+        description: "The bright flavor of basil takes center stage in this pesto lasagna. Paired with a rich cheese sauce and layers of noodles, it's a potluck dish that will have guests asking you for the recipe.",
+        ingredients: [
+            "1/3 cup butter, cubed",
+            "1 medium onion, chopped",
+            "1 garlic clove, minced",
+            "1/2 cup all-purpose flour",
+            "1 teaspoon salt",
+            "3-1/2 cups 2% milk",
+            "4 cups shredded part-skim mozzarella cheese, divided",
+            "1 cup grated Parmesan cheese, divided",
+            "1 teaspoon dried basil",
+            "1 teaspoon dried oregano",
+            "1/2 teaspoon white pepper",
+            "1 carton (15 ounces) whole-milk ricotta cheese",
+            "1 tablespoon minced fresh parsley",
+            "9 lasagna noodles, cooked and drained",
+            "2 packages (10 ounces each) frozen chopped spinach, thawed and squeezed dry",
+            "1 jar (8.1 ounces) prepared pesto"
+        ],
+        instructions: [
+            "Step1. Preheat oven to 350°. In a large saucepan melt butter over medium heat; add onion. Cook and stir until tender, 4-6 minutes. Add garlic; cook 1 minute longer. Stir in flour and salt until blended. Gradually whisk in milk. Bring to a boil; cook and stir until thickened, about 1 minute. Stir in 2 cups mozzarella cheese, 1/2 cup Parmesan cheese, basil, oregano and pepper; set aside.",
+            "Step2. In a large bowl, combine ricotta cheese, parsley and remaining 2 cups mozzarella; set aside. Spread about 1-1/2 cup cheese sauce into a greased 13x9-in. baking dish; top with 3 noodles and half of each of the following: ricotta mixture, pesto and spinach. Repeat layers. Top with remaining noodles and cheese sauce. Sprinkle with remaining 1/2 cup Parmesan.",
+            "Step3. Bake, uncovered, until heated through and cheese is melted and sauce is bubbly, 35-40 minutes. Let stand 15 minutes before serving."
+        ],
         recipe: "Recipe for MainCourse 2",
-        // can add other field here (EX: steps? (can make a array), author, brabrabra)
     },
+
+
+
     {
         id: 5,
         category: "Soup",
-        title: "Soup 1",
-        image: "src/img/food.jpg",
-        article: "https://www.indianhealthyrecipes.com/wp-content/uploads/2019/06/vegetable-soup-500x375.jpg",
+        articleTitle: "Slow-Cooker Butternut Squash Soup",
+        imagePath: "src/assets/recipe/SlowCookerButternutSquashSoup.png",
+        author: "Jennifer Machado",
+        datePublished: "Jun. 27, 2023",
+        description: "Much of the work for this soup can be done in advance, and it keeps all day in the slow cooker. The recipe can easily be doubled if you’re feeding a crowd. Once you’ve tried it, try mixing it up—add sage or savory with the thyme, or replace the thyme with nutmeg. For a vegan version, replace the chicken broth with vegetable broth.",
+        ingredients: [
+            "1 tablespoon olive oil",
+            "1 large onion, chopped",
+            "2 garlic cloves, minced",
+            "1 medium butternut squash (about 4 pounds), peeled and cut into 1-inch piecesr",
+            "1 pound Yukon Gold potatoes (about 2 medium), cut into 3/4-inch pieces",
+            "2 teaspoons minced fresh thyme or 3/4 teaspoon dried thyme",
+            "1 teaspoon salt",
+            "1/4 teaspoon pepper",
+            "5 to 6 cups chicken or vegetable broth",
+            "Sour cream, optional"
+        ],
+        instructions: [
+            "Step1. In a large skillet, heat oil over medium heat. Add onion; saute until tender, 4-5 minutes. Add garlic; cook 1 minute longer. Transfer to a 6-qt. slow cooker. Add next 5 ingredients and 5 cups broth to slow cooker. Cook, covered, on low until vegetables are soft, 6-8 hours.",
+            "Step2. Puree soup using an immersion blender. Or cool slightly and puree soup in batches in a blender; return to slow cooker. Stir in additional broth to reach desired consistency; heat through. If desired, top servings with sour cream."
+        ],
         recipe: "Recipe for Soup 1",
-        // can add other field here (EX: steps? (can make a array), author, brabrabra)
     },
+
+
+
     {
         id: 6,
         category: "Soup",
-        title: "Soup 2",
-        image: "src/img/food.jpg",
-        article: "https://hips.hearstapps.com/hmg-prod/images/chicken-noodle-soup-index-644c2bec1ce0c.jpg?crop=0.888888888888889xw:1xh;center,top&resize=1200:*",
+        articleTitle: "Satisfying Tomato Soup",
+        imagePath: "src/assets/recipe/SatisfyingTomatoSoup.png",
+        author: "Marian Brown",
+        datePublished: "Oct. 05, 2023",
+        description: "After craving tomato soup, I decided to make my own. My sister Joan likes it chunky-style, so she doesn't puree. Serve it with a grilled cheese sandwich.",
+        ingredients: [
+            "2 teaspoons canola oil",
+            "1/4 cup finely chopped onion",
+            "1/4 cup finely chopped celery",
+            "2 cans (14-1/2 ounces each) diced tomatoes, undrained",
+            "1-1/2 cups water",
+            "2 teaspoons brown sugar",
+            "1/2 teaspoon salt",
+            "1/2 teaspoon dried basil",
+            "1/4 teaspoon dried oregano",
+            "1/4 teaspoon coarsely ground pepper",
+            "Minced fresh basil, optional"
+        ],
+
+        instructions: [
+            "Step1. In a large saucepan, heat oil over medium-high heat. Add onion and celery; cook and stir until tender, 2-4 minutes. Add remaining ingredients, except for optional fresh basil. Bring to a boil. Reduce heat; simmer, uncovered, 10 minutes to allow flavors to blend.",
+            "Step2. Puree soup using an immersion blender. Or cool soup slightly and puree in batches in a blender; return to pan and heat through. If desired, top with fresh minced basil."
+        ],
         recipe: "Recipe for Soup 2",
-        // can add other field here (EX: steps? (can make a array), author, brabrabra)
     },
+
+
+
+
     {
         id: 7,
         category: "Dessert",
-        title: "Dessert 1",
-        image: "https://realfood.tesco.com/media/images/RFO-1400x919-classic-chocolate-mousse-69ef9c9c-5bfb-4750-80e1-31aafbd80821-0-1400x919.jpg",
-        article: "Add anything you want here!",
+        articleTitle: "No-Bake Cappuccino Dessert",
+        imagePath: "src/assets/recipe/NoBakeCappuccinoDessert.png",
+        author: "Mary Beth Desens",
+        datePublished: "Jun. 27, 2023",
+        description: "Summer gatherings call for cool desserts like this one. Lots of the party invites I receive come with a request for this delicious treat. You can make it even more chocolaty by adding cocoa powder to the dry pudding mixes.",
+        ingredients: [
+            "15 whole cinnamon graham crackers",
+            "2 packages (8 ounces each) cream cheese, softened",
+            "2 tablespoons instant coffee granules",
+            "3 packages (3.9 ounces each ) instant chocolate pudding mix",
+            "1 teaspoon ground cinnamon, divided",
+            "2-1/2 cups 2% milk",
+            "1 cup cold strong brewed coffee",
+            "8 cups sweetened whipped cream",
+            "3 tablespoons chocolate sprinkles"
+        ],
+
+        instructions: [
+            "Step1. Arrange half of the crackers in a greased 13x9-in. baking dish (breaking crackers to fit). In a large bowl, beat cream cheese, pudding mix, coffee granules and 1/4 teaspoon cinnamon until blended. Gradually beat in milk and coffee until smooth. Fold in 4 cups whipped cream.",
+            "Step2. Spread half of the filling mixture over the crackers. Repeat layers. Top with remaining whipped cream. Refrigerate, covered, 8 hours or overnight. Just before serving, top with sprinkles and remaining cinnamon."
+
+
+        ],
         recipe: "Recipe for Dessert 1",
-        // can add other field here (EX: steps? (can make a array), author, brabrabra)
     },
+
+
+
     {
         id: 8,
         category: "Dessert",
-        title: "Dessert 2",
-        image: "https://realfood.tesco.com/media/images/RFO-1400x919-classic-chocolate-mousse-69ef9c9c-5bfb-4750-80e1-31aafbd80821-0-1400x919.jpg",
-        article: "Add anything you want here!",
-        recipe: "Recipe for Dessert 2",
-        // can add other field here (EX: steps? (can make a array), author, brabrabra)
-    }
-]
+        articleTitle: "Quick and Easy Tiramisu",
+        imagePath: "src/assets/recipe/QuickandEasyTiramisu.png",
+        author: "Taste of Home Test Kitchen",
+        datePublished: "Sep. 13, 2023",
+        description: "No one can resist this quick and easy tiramisu recipe. Make it ahead for added mealtime convenience.",
+        ingredients: [
+            "2 cups cold 2% milk",
+            "1 package (3.4 ounces) instant vanilla pudding mix",
+            "1 cup heavy whipping cream",
+            "3 tablespoons confectioners' sugar",
+            "28 soft ladyfingers, split",
+            "2-1/2 teaspoons instant coffee granules",
+            "1/2 cup boiling water",
+            "1 tablespoon baking cocoa"
+        ],
 
+        instructions: [
+            "Step1. In a large bowl, whisk milk and pudding mix for 2 minutes. Let stand until soft-set, about 2 minutes. In a small bowl, beat cream until it begins to thicken. Add confectioners' sugar; beat until soft peaks form. Fold into pudding; cover and refrigerate.",
+            "Step2. Arrange half the ladyfingers cut side up in an 11x7-in. dish. Dissolve coffee granules in the boiling water; drizzle half over the ladyfingers. Spread with half the pudding mixture. Repeat layers. Sprinkle with cocoa. Refrigerate until serving."
+        ],
+
+        recipe: "Recipe for Dessert 2",
+
+    },
+
+
+]
