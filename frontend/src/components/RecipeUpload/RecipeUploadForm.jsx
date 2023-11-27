af7f45a06a7d0aba865efeeedd75270f54ecc910
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2023-11-26 16:22:38 -0800

    Backup

--- frontend/src/components/RecipeUpload/RecipeUploadForm.jsx
+++ frontend/src/components/RecipeUpload/RecipeUploadForm.jsx
@@ -2,25 +2,105 @@ import React, { useState } from 'react';
 import './RecipeUploadForm.css';
 
 function RecipeUploadForm() {
+    const [userEmail, setUserEmail] = useState(''); // Added state for user email
     const [title, setTitle] = useState('');
     const [instructions, setInstructions] = useState('');
     const [image, setImage] = useState(null);
 
-    const handleSubmit = (event) => {
+    function handleFileChange(e) {
+        const file = e.target.files[0];
+        if (file) {
+            setImage(file);
+        }
+    }
+
+    async function handleSubmit(event) {
         event.preventDefault();
-        // Here you would handle the form submission,
-        // including file upload which you can implement
-        // by sending a POST request to your backend server
-    };
+
+        // Check if image is selected
+        if (!image) {
+            console.error('No image selected');
+            return; // or handle this scenario appropriately
+        }
+
+        // Split instructions into an array of strings based on newline
+        const instructionsList = instructions.split('\n')
+            .map(line => line.trim()) // Trim each line
+            .filter(line => line); // Filter out empty strings
+
+        // Convert the image to base64
+        const reader = new FileReader();
+        reader.readAsDataURL(image);
+
+        reader.onload = async () => {
+            // Ensure reader.result is a string before appending
+            const base64Image = reader.result;
+            if (typeof base64Image !== 'string') {
+                console.error('FileReader result is not a string');
+                return;
+            }
+
+            // Prepare the form data
+            const data = {
+                user_email: userEmail,
+                title: title,
+                instructions: instructionsList,
+                image: reader.result
+            };
+
+            console.log(data);
+
+            try {
+                // Send the POST request
+                console.log(data);
+                const response = await fetch('http://10.40.134.55:3000/api/recipes', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    // body: data
+                    body: JSON.stringify({
+                        user_email: userEmail,
+                        title: title,
+                        instructions: instructionsList,
+                        image: reader.result
+                    })
+                });
+
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+
+                // Handle the response
+                const responseData = await response.json();
+                console.log('Upload successful', responseData);
+            } catch (error) {
+                console.error('There was a problem with the fetch operation:', error);
+            }
+        };
+
+        reader.onerror = () => {
+            console.error('Error occurred reading the file:', reader.error);
+        };
+    }
 
     return (
         <div className="recipe-upload-form">
             <div className="upload-label">Import Your Recipe</div>
             <div className="upload-btn-wrapper">
                 <button>Upload Image</button>
-                <input type="file" name="myfile" onChange={(e) => setImage(e.target.files[0])} />
+                <input type="file" name="myfile" onChange={handleFileChange} />
             </div>
             <form onSubmit={handleSubmit}>
+                <label htmlFor="user-email">User Email:</label>
+                <input
+                    id="user-email"
+                    type="email" // Set type to email for proper validation
+                    value={userEmail}
+                    onChange={(e) => setUserEmail(e.target.value)}
+                    required
+                />
+
                 <label htmlFor="title">Recipe Title:</label>
                 <input
                     id="title"
