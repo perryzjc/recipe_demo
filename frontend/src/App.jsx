fb4013a561d10a9e1c30367f3869347a9369ece3
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2023-11-29 18:04:47 -0800

    Backup animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import Appetizer from "./pages/Appetizer";
@@ -9,46 +9,62 @@ import Soup from "./pages/Soup";
 import Dessert from "./pages/Dessert";
 import Recipe from "./pages/Recipe";
 import RecipeUploadForm from './components/RecipeUpload';
+import IntroAnimation from './components/IntroAnimation'; // Import your intro animation component
 
 import getContents from "./utils/getContent.js";
 
 function App() {
     const [contents, setContents] = useState([]);
+    const [showIntro, setShowIntro] = useState(false);
 
     useEffect(() => {
+        // Check if the intro animation has been shown
+        if (!localStorage.getItem("introShown")) {
+            setShowIntro(true);
+        }
+
         async function fetchData() {
             const data = await getContents();
             console.log('Loading data...');
             console.log(data);
-          if (data instanceof Uint8Array) {
-            // Convert Uint8Array to regular array
-            setContents(Array.from(data));
-          } else {
-            setContents(data);
-          }
+            if (data instanceof Uint8Array) {
+                setContents(Array.from(data));
+            } else {
+                setContents(data);
+            }
         }
 
         fetchData().then(() => {
-            console.log('Data loaded successfully kjasfkjaskfjaskjfkasjfkasjfka');
+            console.log('Data loaded successfully');
         });
     }, []);
 
+    // Function to hide the intro and set the flag in local storage
+    const hideIntro = () => {
+        localStorage.setItem("introShown", "true");
+        setShowIntro(false);
+    };
+
     return (
         <>
-            <Router basename="/recipe_demo">
-                <div>
-                    <Navbar/>
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="/appetizer" element={<Appetizer contents={contents}/>}/>
-                        <Route path="/maincourse" element={<MainCourse/>}/>
-                        <Route path="/soup" element={<Soup/>}/>
-                        <Route path="/dessert" element={<Dessert/>}/>
-                        <Route path="/recipe/:id" element={<Recipe/>}/>
-                        <Route path="/upload" element={<RecipeUploadForm/>}/>
-                    </Routes>
-                </div>
-            </Router>
+            {showIntro ? (
+                <IntroAnimation onEnd={hideIntro} />
+            ) : (
+                <Router basename="/recipe_demo">
+                    <div>
+                        <Navbar />
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/appetizer" element={<Appetizer contents={contents} />} />
+                            <Route path="/maincourse" element={<MainCourse />} />
+                            <Route path="/soup" element={<Soup />} />
+                            <Route path="/dessert" element={<Dessert />} />
+                            <Route path="/recipe/:id" element={<Recipe />} />
+                            <Route path="/upload" element={<RecipeUploadForm />} />
+                        </Routes>
+                    </div>
+                </Router>
+            )}
         </>
     );
 }
---
+++ frontend/src/components/IntroAnimation.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { startAnimation } from '../utils/introAnimation.js'; // Import from animation.js
+
+const IntroAnimation = ({ onEnd }) => {
+    useEffect(() => {
+        // Initialize your animation
+        const animationControl = startAnimation();
+
+        // When the component unmounts or animation ends
+        return () => {
+            onEnd(); // Call the onEnd callback
+
+            // If there's any cleanup function in your animation.js, call it here
+            // anotherFunction();
+        };
+    }, [onEnd]);
+
+    return <div id="intro-animation-container"></div>;
+};
+
+export default IntroAnimation;
