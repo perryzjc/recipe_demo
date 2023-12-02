63e11a3304978b795607c0d564e97dc67d30b924
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2023-12-01 19:49:20 -0800

    Update resolveImagePath.js

--- frontend/src/utils/resolveImagePath.js
+++ frontend/src/utils/resolveImagePath.js
@@ -1,5 +1,9 @@
 export function resolveImagePath(relativePath) {
     const baseUrl = import.meta.env.VITE_APP_BASE_URL || ''; // for Vite
     // const baseUrl = process.env.REACT_APP_BASE_URL || ''; // for Create React App
+    console.log("I am in resolveImagePath")
+    console.log(`baseUrl: ${baseUrl}`)
+    console.log(`relativePath: ${relativePath}`)
+    // test comment
     return `${baseUrl}${relativePath}`;
 }
